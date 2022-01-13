7203af605cdadf34d39315bb6eee5b994aaa4a37
gv68682 <gv68682@gmail.com>
2022-01-13 16:51:46 -0600

    formik app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useFormik} from 'formik'
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: ''      
+    },
+    onSubmit: values =>{
+      console.log('form:',values);
+    },
+    validate: values =>{
+      let errors = {};
+      if(!values.name) errors.name = 'Required';
+      if(!values.email) errors.email = 'Required';
+      if(!values.password) errors.password = 'Required';
+      return errors;
+    }
+  });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Name:</div>
+        <input type="text" name="name" onChange={formik.handleChange} value={formik.values.name}/>
+        {formik.errors.name ? <div style={{color:'red'}}>{formik.errors.name}</div> : null}
+        <div>Email:</div>
+        <input type="text" name="email" id="emailField" onChange={formik.handleChange} value={formik.values.email}/>
+        {formik.errors.email ? <div id ="emailError" style={{color:'red'}}>{formik.errors.email}</div> : null}        
+        <div>Password:</div>
+        <input type="text" name="password" id="pswField" onChange={formik.handleChange} value={formik.values.password}/><br/>
+        {formik.errors.password ? <div id="pswError" style={{color:'red'}}>{formik.errors.password}</div> : null}                
+        <button type="submit">Submit</button>
+      </form>      
     </div>
   );
 }
